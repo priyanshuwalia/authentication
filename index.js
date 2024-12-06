9d28579fa5d00e5b4b1419fad593ddf0e742c2e9
Priyanshu Walia <waliapriyanshu07@gmail.com>
2024-12-06 23:39:14 +0530

    adding files

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require (`express`);
+const app = express();
+const port = 3000;
+
+app.post(`/signup`, function(req,res){
+    
+})
+app.post(`/signin`, function(req,res){
+    
+})
+app.listen(port, ()=>{
+    console.log(`this is running on port ${port}`)
+})
