65480382381bde67b3990363ec96ad3de4dc747f
Priyanshu Walia <waliapriyanshu07@gmail.com>
2024-12-06 23:42:50 +0530

    this message is for modifying git repo

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require (`express`);
 const app = express();
 const port = 3000;
+app.use(express.json());
+
+const users = [];
 
 app.post(`/signup`, function(req,res){
+    const username= req.body.username;
+    const password= req.body.password;
+
+    users.push({
+        username:username,
+        password:password
+    })
     
+    res.json({
+        message: "You are signed in"
+    })
 })
 app.post(`/signin`, function(req,res){
     
