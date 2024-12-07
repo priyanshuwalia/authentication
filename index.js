30253b8d49a3b76ec20ffafe35fdf99188ed6f6f
Priyanshu Walia <waliapriyanshu07@gmail.com>
2024-12-07 20:08:11 +0530

    addded some more code for authentication

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const port = 3000;
 app.use(express.json());
 
 const users = [];
+// should return a  token. thats it! 
+function generateToken(){
+    let options = [`a`, `b`, `d`, `e`, `f`, `g`, `h`, `i`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`, `u`, `v`, `w`, `x`, `y`, `z`]
+    let token= "";
+    for(let i; i<32; i++){
+        token= token+options[Math.floor(Math.random()* options.length)];
+    }
+    return token;
+}
+
 
 app.post(`/signup`, function(req,res){
     const username= req.body.username;
@@ -19,6 +29,16 @@ app.post(`/signup`, function(req,res){
     })
 })
 app.post(`/signin`, function(req,res){
+    const username= req.body.username;
+    const password= req.body.password;
+
+    const user = users.find(function(u){
+        if (u.username== username && u.password == password){
+            return true;
+        } else{
+             return false;
+        }
+    })
     
 })
 app.listen(port, ()=>{
