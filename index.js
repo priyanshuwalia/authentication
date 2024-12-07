1461511827dfa870157f913a31c079860d2dd38e
Priyanshu Walia <waliapriyanshu07@gmail.com>
2024-12-07 20:18:40 +0530

    modified the authentication code

--- index.js
+++ index.js
@@ -1,46 +1,59 @@
-const express = require (`express`);
+const express = require("express");
 const app = express();
 const port = 3000;
+
 app.use(express.json());
 
 const users = [];
-// should return a  token. thats it! 
-function generateToken(){
-    let options = [`a`, `b`, `d`, `e`, `f`, `g`, `h`, `i`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`, `u`, `v`, `w`, `x`, `y`, `z`]
-    let token= "";
-    for(let i; i<32; i++){
-        token= token+options[Math.floor(Math.random()* options.length)];
+
+// Function to generate a random 32-character token
+function generateToken() {
+    let options = "abcdefghijklmnopqrstuvwxyz";
+    let token = "";
+    for (let i = 0; i < 32; i++) {
+        token += options[Math.floor(Math.random() * options.length)];
     }
     return token;
 }
 
-
-app.post(`/signup`, function(req,res){
-    const username= req.body.username;
-    const password= req.body.password;
+// Signup route
+app.post("/signup", function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
 
     users.push({
-        username:username,
-        password:password
-    })
-    
-    res.json({
-        message: "You are signed in"
-    })
-})
-app.post(`/signin`, function(req,res){
-    const username= req.body.username;
-    const password= req.body.password;
+        username: username,
+        password: password
+    });
 
-    const user = users.find(function(u){
-        if (u.username== username && u.password == password){
-            return true;
-        } else{
-             return false;
-        }
-    })
-    
-})
-app.listen(port, ()=>{
-    console.log(`this is running on port ${port}`)
-})
+    res.json({
+        message: "You are signed up!"
+    });
+});
+
+// Signin route
+app.post("/signin", function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const foundUser = users.find(function (u) {
+        return u.username === username && u.password === password;
+    });
+
+    if (foundUser) {
+        const token = generateToken();
+        res.json({
+            message: "You are signed in!",
+            token: token
+        });
+    } else {
+        res.status(401).json({
+            message: "Invalid username or password."
+        });
+    }
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
